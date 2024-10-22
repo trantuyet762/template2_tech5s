49a62ea4f6e9b6235382af788eda5c207f58509f
trantuyet762 <trantuyet2203@gmail.com>
2024-10-22 11:02:15 +0700

    add res section

--- public/js/main.js
+++ public/js/main.js
@@ -49,4 +49,34 @@ var swiper_img_customer = new Swiper(".swiper_img_customer_container", {
     
    
   });
-  
+
+ /// toggle
+const toggleIcons = document.querySelectorAll('.toggle-icon');
+const toggleContents = document.querySelectorAll('.toggle-content');
+
+toggleIcons.forEach((icon, index) => {
+    icon.addEventListener('click', () => {
+     
+        toggleContents.forEach((content, i) => {
+            if (i !== index) {
+                content.classList.add('hidden'); 
+                content.classList.remove('flex'); 
+            }
+        });
+
+        const content = toggleContents[index];
+        const svgPath = icon.querySelector('path');
+
+        if (content.classList.contains('hidden')) {
+            content.classList.remove('hidden'); 
+            content.classList.add('flex'); 
+            svgPath.setAttribute('d', 'M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM6.75 9.25a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Z');
+
+           
+        } else {
+            content.classList.add('hidden'); 
+            content.classList.remove('flex'); 
+            svgPath.setAttribute('d', 'M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z');
+        }
+    });
+});
