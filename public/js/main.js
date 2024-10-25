88455d8466d684968287d3af4fa1e2be9606b9d1
trantuyet762 <trantuyet2203@gmail.com>
2024-10-25 12:03:12 +0700

    add section

--- public/js/main.js
+++ public/js/main.js
@@ -80,3 +80,18 @@ toggleIcons.forEach((icon, index) => {
         }
     });
 });
+
+///swiper table_price
+
+var swiper_table_service = new Swiper(".swiper_table_service_container", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
